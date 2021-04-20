4313f27ec577867651e65a7d1b7ba8abb08067a2
Eshan Parmar <eshan.parmar@gmail.com>
2021-04-20 02:59:05 -0400

    Update Skills

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -92,24 +92,24 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
+     { name: "Java", value: 90 },
+    { name: "Python", value: 60 },
     { name: "Data Structures", value: 85 },
     { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
+    { name: "JavaScript", value: 80 },
     { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "HTML/CSS", value: 85 },
+    { name: "Matlab", value: 75 },
   ],
   softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
+    { name: "Goal-Oriented", value: 95 },
+    { name: "Collaboration", value: 100 },
+    { name: "Positivity", value: 85 },
     { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
+    { name: "Problem Solving", value: 95 },
     { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
+    { name: "Organization", value: 90 },
+    { name: "Creativity", value: 80 },
   ],
 };
 
@@ -118,7 +118,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+    "I'm currently looking for full-time Software Engineering positions startin in 2022! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
   email: "eshan.parmar@gmail.com",
 };
 
